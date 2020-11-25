43f4c4cc62bd6d8a18a1ac89506a19ea4d238b1e
mattj299 <mateonas586@gmail.com>
2020-11-24 18:25:11 -0800

    updated to final project.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ function Footer(props) {
   return (
     <footer className="footer">
       <p>
-        This project is just a replica of an existing project. <br></br> It's
-        purpose is for me to code and get used to using react.js
+        The purpose of this project is for me to code and get used to using
+        react.js
       </p>
       <p>
         Built using <a href="https://reactjs.org/">react.js</a>
